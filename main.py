cd1f2600fcc53f5685190df808ecfc4827066b6d
ML Club <bhsml2020@gmail.com>
2020-11-20 17:41:58 -0800

    $assign_assignment developed and tested

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import discord
-from webserver import keep_alive
-import config as c
-import os
+import requests
+import json
+from configparser import ConfigParser
+from os import listdir
+from os.path import isfile
+
+config = ConfigParser()
+config.read('config.ini')
+
+token = config.get('data', 'token')
+StudentRole = config.getint('roles', 'StudentRole')
+AdvisorRole = config.getint('roles', 'AdvisorRole')
+AssignmentsPath = config.get('folders', 'assignmentsPath')
+
+helpersPath = config.get('folders', 'HelpersPath')
 
 client = discord.Client()
-roles = {'Student': 778053523187040297,
-         'Advisor': 778053252431609868}
 
 
 @client.event
@@ -19,15 +29,28 @@ async def on_message(message):
         return
 
     elif message.content.startswith('$help'):
-        await message.channel.send('identifier $ ')
+        files = {}
+        for i in listdir(helpersPath):
+            if isfile(helpersPath + i):
+                with open(helpersPath + i, 'r') as f:
+                    files[i] = f.read()
+                    f.close()
+        for i in message.author.roles:
+            if i.id == StudentRole:
+                text = files['StudentCommands.txt']
+                await message.channel.send(f'```{text}```')
+
+            elif i.id == AdvisorRole:
+                text = files['AdvisorCommands.txt']
+                await message.channel.send(f'```{text}```')
 
     elif message.content.startswith('$register'):
         for i in message.author.roles:
-            if i.id == roles['Student']:
+            if i.id == StudentRole:
                 await message.channel.send("You already have the Student role. "
                                            "If you want to switch, message a moderator.")
                 return
-            elif i.id == roles['Advisor']:
+            elif i.id == AdvisorRole:
                 await message.channel.send("You already have the advisor role")
                 return
 
@@ -38,17 +61,47 @@ async def on_message(message):
         except discord.Forbidden:
             await message.channel.send('I don\'t have the permission to do that, contact a moderator')
 
-    elif message.content.startswith('$check_assignment'):
-        await message.channel.send('Command has not been coded yet')
+    elif message.content.startswith('$assign_assignment'):
+        with open('Database/assignments.json', 'r') as f:
+            assignments = json.load(f)
+            f.close()
+
+        if len(message.attachments) > 0:
+            description = message.content[18:]
+            for file in message.attachments:
+                if len(description) < 1:
+                    await message.channel.send('Please provide a description \n'
+                                               '$assign_assignment [description]')
+                    return
+                elif '.txt' not in file.filename:
+                    await message.channel.send('Send a .txt file!')
+                    return
+                else:
+                    # Downloading and saving the uploaded file
+                    r = requests.get(file.url)
+                    with open(AssignmentsPath+file.filename, 'w') as f:
+                        f.write(str(r.content))
+                        f.close()
+
+                    # updating the database
+                    assignments['AssignmentFileName'] = file.filename
+                    assignments['description'] = description
+
+                    await message.channel.send('the file has been downloaded and recorded in the database')
+        else:
+            await message.channel.send('Attach a text file!')
+        with open('Database/assignments.json', 'w') as f:
+            json.dump(assignments, f)
+            f.close()
 
     elif message.content.startswith('$submit_assignment'):
         await message.channel.send('Command has not been coded yet')
 
-    elif message.content.startswith('$assign_assignment'):
+    elif message.content.startswith('$check_assignment'):
         await message.channel.send('Command has not been coded yet')
 
     elif message.content.startswith('$assignment_progress'):
         await message.channel.send('Command has not been coded yet')
 
-keep_alive()
-client.run(c.token)
+
+client.run(token)
